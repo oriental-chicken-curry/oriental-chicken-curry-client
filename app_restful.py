6789bd35a9f54ce6b47a2ba4084930fa6f7fa918
gan-ta <gilmat@naver.com>
2021-06-01 17:01:56 +0900

    통신 코드 작업

--- app_restful.py
+++ app_restful.py
@@ -38,4 +38,4 @@ api.add_resource(MainPage, '/')
 api.add_resource(CropPage, '/crop', endpoint='crop' )
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(debug=True, host='0.0.0.0', port=81)
