b535f4abb272eeaf4e9f4af274b64d5250354e8d
Ubuntu <ubuntu@ip-172-31-8-13.ap-northeast-2.compute.internal>
2021-06-07 17:55:43 +0000

    배포 수정

--- app_restful.py
+++ app_restful.py
@@ -38,4 +38,4 @@ api.add_resource(MainPage, '/')
 api.add_resource(CropPage, '/crop', endpoint='crop' )
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=81)
+    app.run(debug=True, host='0.0.0.0', port=8082)
